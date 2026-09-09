ae6d8d2701ebf1b24423efde2dff106f88084706
tigelaa <tigelaa@proton.me>
2026-09-09 19:17:20 -0300

    ali: add missing cs35l35_pdm_params for Madera amp link

--- techpack/audio/asoc/msm8952-dai-links.c
+++ techpack/audio/asoc/msm8952-dai-links.c
@@ -377,6 +377,14 @@ static const struct snd_soc_pcm_stream cs35l34_params = {
 	.channels_max = 2,
 };
 
+static const struct snd_soc_pcm_stream cs35l35_pdm_params = {
+	.formats = SNDRV_PCM_FORMAT_S16_LE,
+	.rate_min = 96000,
+	.rate_max = 96000,
+	.channels_min = 1,
+	.channels_max = 2,
+};
+
 static struct snd_soc_dai_link msm8952_madera_l34_dai_link[] = {
 	{
 		.name = "MADERA-AMP",
